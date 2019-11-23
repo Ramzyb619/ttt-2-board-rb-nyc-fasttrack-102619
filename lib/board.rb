f7b4ee121d59a034947df786b471ea0cf0c70b9f
Ramzy Badawy <ramzy.badawy@gmail.com>
2019-11-23 15:58:47 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+puts Student 1 = "Ramzy"
+puts Student 2 = "Natalie"
+puts Student 3 = "Chris"
+puts Student 4 = "Dave"
+puts Student 5 = "John"
+puts Student 6 = "Danny"
+puts Student 7 = "Timmy"
+puts Student 8 = "CJ"
+puts Student 9 = "Jesse" 
+
+end 
